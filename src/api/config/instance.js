23a9fe6c1b68c2c13be704d187228ac3fcece141
DaLVeRS2001 <vladbiricov2@gmail.com>
2025-01-22 03:40:38 +0300

    fixes

--- src/api/config/instance.js
+++
@@ -1,9 +0,0 @@
-import * as axios from "axios";
-
-const instance = axios.create({
-	withCredentials: true,
-	headers: {'API-KEY': '655c325f-f4b3-44cd-8c93-20f2b75dddb0'},
-	baseURL: 'https://social-network.samuraijs.com/api/1.0'
-})
-
-export default instance
